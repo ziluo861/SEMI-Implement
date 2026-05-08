c1dd1089ecab51f1ab5ea09010ea9733c0b8bf03
ziluo861 <727559810@qq.com>
2026-05-08 20:57:05 +0800

    upgrade SECSMessageHandleItem

--- E5/include/SECSMessageHandleItem.hpp
+++ E5/include/SECSMessageHandleItem.hpp
@@ -3,23 +3,24 @@
 #include "SECS/SECSBase.hpp"
 #include "SECSHead/SECSHead.hpp"
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 class SECSMessageHandleItem {
 private:
   std::vector<std::uint8_t> remote_head_bytes_;
-  SECSHead *request_head_ = nullptr;
-  SECSItemBase *request_item_ = nullptr;
-  SECSItemBase *reply_item_ = nullptr;
+  std::shared_ptr<SECSHead> request_head_ = nullptr;
+  std::shared_ptr<SECSItemBase> request_item_ = nullptr;
+  std::shared_ptr<SECSItemBase> reply_item_ = nullptr;
 
 public:
-  static SECSItemBase *RejectItem() noexcept;
-  const SECSHead *RequestHead() const noexcept;
-  const SECSItemBase *RequestItem() const noexcept;
+  SECSItemBase * RejectItem() noexcept;
+  std::shared_ptr<SECSHead> RequestHead() const noexcept;
+  std::shared_ptr<SECSItemBase> RequestItem() const noexcept;
   const std::vector<std::uint8_t> &RemoteHeadBytes() const noexcept;
-  SECSHead ReplyHead() noexcept;
-  const SECSItemBase *ReplyItem() const noexcept;
-  void Set_ReplyItem(SECSItemBase *) noexcept;
+  std::shared_ptr<SECSHead> ReplyHead() noexcept;
+  std::shared_ptr<SECSItemBase> ReplyItem() const noexcept;
+  void Set_ReplyItem(std::shared_ptr<SECSItemBase>) noexcept;
 
-  explicit SECSMessageHandleItem(SECSHead *, SECSItemBase *);
+  explicit SECSMessageHandleItem(std::shared_ptr<SECSHead>, std::shared_ptr<SECSItemBase>);
 };
